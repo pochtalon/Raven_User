4542a1a265266785f8d0d70f046280c795dd7339
pochtalon <pochtalon1010@gmail.com>
2024-01-24 16:27:45 +0200

    Created

---
+++ src/test/java/com/example/raven_user/RavenUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raven_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavenUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
